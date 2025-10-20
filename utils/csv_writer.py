0f2cf1a9bc282948d12f936ea178b6267b1141a6
MatteoSvg <matteosolavagione04@gmail.com>
2025-10-20 17:48:57 +0200

    aggiunta riga di codice per creare la cartella se non esiste prima di scrivere i dati nei file csv

--- utils/csv_writer.py
+++ utils/csv_writer.py
@@ -1,6 +1,10 @@
 import csv
+import os
 
 def export_recommendations(db):
+    # Crea la cartella data se non esiste
+    os.makedirs("data", exist_ok=True)
+    
     recommendations = db.find_recommendations()
     with open("data/recommendations.csv", mode="w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
@@ -8,11 +12,12 @@ def export_recommendations(db):
         # Scrive ogni tupla come una riga nel file
         writer.writerows(recommendations)
 
-
 def export_stocks(stocks_list):
+    # Crea la cartella data se non esiste
+    os.makedirs("data", exist_ok=True)
+    
     with open("data/ftse.csv", mode="w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow(["name", "market_price"])
         for stock in stocks_list:
             writer.writerow([stock[1], stock[2]])
-        
