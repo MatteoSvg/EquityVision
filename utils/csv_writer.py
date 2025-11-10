48370e0c215b2f9a7c9f6508097bca47641c318f
Matteo Solavagione <matteosolavagione04@gmail.com>
2025-11-10 11:32:29 +0100

    aggiunti commenti con docstring per csv_writer.py

--- utils/csv_writer.py
+++ utils/csv_writer.py
@@ -1,31 +1,15 @@
-# import csv
-# import os
-
-# def export_recommendations(db):
-#     # Crea la cartella data se non esiste
-#     os.makedirs("data", exist_ok=True)
-    
-#     recommendations = db.find_recommendations()
-#     with open("data/recommendations.csv", mode="w", newline="", encoding="utf-8") as f:
-#         writer = csv.writer(f)
-#         writer.writerow(["name", "bank", "target_price", "date"])
-#         # Scrive ogni tupla come una riga nel file
-#         writer.writerows(recommendations)
-
-# def export_stocks(stocks_list):
-#     # Crea la cartella data se non esiste
-#     os.makedirs("data", exist_ok=True)
-    
-#     with open("data/ftse.csv", mode="w", newline="", encoding="utf-8") as f:
-#         writer = csv.writer(f)
-#         writer.writerow(["name", "market_price"])
-#         for stock in stocks_list:
-#             writer.writerow([stock[1], stock[2]])
-
 import csv
 import os
 
 def export_recommendations(db):
+    '''
+    Funzione: export_recommendations
+    Esporta le raccomandazioni trovate nel database in un file CSV
+    Parametri formali:
+    object db -> Oggetto database da cui estrarre le raccomandazioni
+    Valore di ritorno:
+    None -> Nessun valore di ritorno.
+    '''
     # Crea la cartella data se non esiste
     os.makedirs("data", exist_ok=True)
     
@@ -36,12 +20,20 @@ def export_recommendations(db):
     
     recommendations = db.find_recommendations()
     with open(file_path, mode="w", newline="", encoding="utf-8") as f:
-        writer = csv.writer(f)
+        writer = csv.writer(f) #mi salvo l'oggetto writer per scrivere sul file
         writer.writerow(["name", "bank", "target_price", "date"])
         # Scrive ogni tupla come una riga nel file
         writer.writerows(recommendations)
 
 def export_stocks(stocks_list):
+    '''
+    Funzione: export_stocks
+    Esporta una lista di azioni (titoli) in un file CSV
+    Parametri formali:
+    list stocks_list -> Lista di azioni da esportare. Ogni elemento è una tupla/lista contenente i dati del titolo.
+    Valore di ritorno:
+    None -> Nessun valore di ritorno.
+    '''
     # Crea la cartella data se non esiste
     os.makedirs("data", exist_ok=True)
     
@@ -51,7 +43,7 @@ def export_stocks(stocks_list):
         os.remove(file_path)
     
     with open(file_path, mode="w", newline="", encoding="utf-8") as f:
-        writer = csv.writer(f)
+        writer = csv.writer(f) 
         writer.writerow(["name", "market_price"])
         for stock in stocks_list:
             writer.writerow([stock[1], stock[2]])
